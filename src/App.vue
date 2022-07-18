d12905907bdfc42b0375fcd91776be8bfd9ba52b
skeito <s.keito896@gmail.com>
2022-07-18 12:23:34 +0900

    Add responsive-design

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,80 @@
         <v-divider></v-divider>
         <!-- ナビゲーションリスト項目 -->
         <v-list dense nav>
-          <v-list-group
-          v-for="nav_list in nav_lists"
-          :key="nav_list.name"
-          :prepend-icon="nav_list.icon"
-          no-action
-          :append-icon="nav_list.lists ? undefined : ''">
-          <template v-slot:activator>
-            <v-list-item-content>
-              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
-            </v-list-item-content>
-          </template>
-          <v-list-item v-for="list in nav_list.lists" :key="list">
-            <v-list-item-content>
-              <v-list-item-title>{{ list }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          </v-list-group>
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-vuetify</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollProfile()" class="list-item">Profile</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-briefcase</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollWork()" class="list-item">Work</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-format-list-bulleted-square</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollBackground()" class="list-item">Background</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-head-heart</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollLikes()" class="list-item">Likes</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-remote-desktop</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollSkill()" class="list-item">Skill</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-group no-action>
+            <template v-slot:activator>
+            <v-list-item class="pl-0">
+              <v-list-item-icon><v-icon>mdi-book-open-page-variant</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollPortofolio()" class="list-item">Portofolio</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            </template>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="list-item" @click="moveIsara()">iSara</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="list-item" @click="moveLancers()">Lancers</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="list-item" @click="moveLikson()">LIKSON</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            </v-list-group>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-account-wrench</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="scrollServices()" class="list-item">Services</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-github</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title @click="moveGithub()" class="list-item">Source</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
         </v-list>
       </v-container>
     </v-navigation-drawer>
@@ -39,7 +96,7 @@
       <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Potofolio Site</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="navbar-item">
           <v-btn id="profile_nav" text>Profile</v-btn>
           <v-btn id="work_nav" text>Work</v-btn>
           <v-btn id="background_nav" text>Background</v-btn>
@@ -54,7 +111,7 @@
       <router-view />
     </v-main>
     <v-footer color="primary" dark app>
-      Portofolio Site
+      <v-btn id="portofolio-top" text>Portofolio Site</v-btn>
     </v-footer>
   </v-app>  
 </template>
@@ -66,20 +123,55 @@ export default {
     data() {
       return {
         drawer: null,
-        supports: [
-          'iSara',
-          'Lancers',
-          'LIKSON',
-        ],
-        nav_lists: [
-          {name: 'Profile', icon: 'mdi-vuetify'},
-          {name: 'Skill', icon: 'mdi-remote-desktop', lists: ['HTML', 'CSS', 'JavaScript', 'PHP', 'Laravel', 'Vue.js', 'Nuxt.js', 'Sass', 'jQuery', 'Bootstrap', 'Vuetify', 'WordPress']},
-          {name: 'Work', icon: 'mdi-briefcase', lists: ['iSara', 'Lancers', 'LIKSON']},
-          {name: 'Service', icon: 'mdi-account-wrench', lists: ['Coding', 'WordPress', 'Web Development']},
-          {name: 'Source', icon: 'mdi-github'},
-        ]
       }
     },
+    methods: {
+      moveIsara: function() {
+        let url = 'https://tarao96.github.io/iSara/'
+        window.location.href = url
+      },
+      moveLancers: function() {
+        let url = 'https://tarao96.github.io/Lancers/'
+        window.location.href = url
+      },
+      moveLikson: function() {
+        let url = 'https://tarao96.github.io/LIKSON/'
+        window.location.href = url
+      },
+      moveGithub: function() {
+        let url = 'https://github.com/tarao96/myportofolio-vue'
+        window.location.href = url
+      },
+      scrollProfile: function() {
+          var profileLocate = $('#profile_locate').offset().top;
+          $(window).scrollTop(profileLocate - 150);
+      },
+      scrollWork: function() {
+        var workLocate = $('#work_locate').offset().top;
+        $(window).scrollTop(workLocate - 150);
+      },
+      scrollBackground: function() {
+        var backgroundLocate = $('#background_locate').offset().top;
+        $(window).scrollTop(backgroundLocate - 150);
+      },
+      scrollLikes: function() {
+        var likesLocate = $('#likes_locate').offset().top;
+        $(window).scrollTop(likesLocate - 150);
+      },
+      scrollSkill: function() {
+        var skillLocate = $('#skill_locate').offset().top;
+        $(window).scrollTop(skillLocate - 150);
+      },
+      scrollPortofolio: function() {
+        var portofolioLocate = $('#portofolio_locate').offset().top;
+        $(window).scrollTop(portofolioLocate - 150);
+      },
+      scrollServices: function() {
+        var servicesLocate = $('#services_locate').offset().top;
+        $(window).scrollTop(servicesLocate);
+      }
+    },
+
     mounted: function() {
       var profileLocate = $('#profile_locate').offset().top;
       var workLocate = $('#work_locate').offset().top;
@@ -90,32 +182,39 @@ export default {
       var servicesLocate = $('#services_locate').offset().top;
 
       $('#profile_nav').click(function() {
-          $(window).scrollTop(profileLocate);
+          $(window).scrollTop(profileLocate - 150);
       });
 
       $('#work_nav').click(function() {
-          $(window).scrollTop(workLocate);
+          $(window).scrollTop(workLocate - 150);
       });
 
       $('#background_nav').click(function() {
-          $(window).scrollTop(backgroundLocate);
+          $(window).scrollTop(backgroundLocate - 150);
       });
 
       $('#likes_nav').click(function() {
-          $(window).scrollTop(likesLocate);
+          $(window).scrollTop(likesLocate - 150);
       });
 
       $('#skill_nav').click(function() {
-          $(window).scrollTop(skillLocate);
+          $(window).scrollTop(skillLocate - 150);
       });
 
       $('#portofolio_nav').click(function() {
-          $(window).scrollTop(portofolioLocate);
+          $(window).scrollTop(portofolioLocate - 150);
       });
 
       $('#services_nav').click(function() {
           $(window).scrollTop(servicesLocate);
       });
+
+      $('#portofolio-top').click(function() {
+                $('body, html').animate({
+                    scrollTop: 0
+                }, 500);
+                return false;
+            });
 
       $('.work-item').hover(function() {
                 $('.caption', this).animate({
@@ -129,3 +228,15 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.list-item {
+  cursor: pointer;
+}
+
+@media(max-width: 480px) {
+  .navbar-item {
+    display: none;
+}
+}
+</style>
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -3,107 +3,107 @@
     <div class="article mt-10 mb-10">
     <v-row>
         <v-spacer></v-spacer>
-            <v-col cols="6" class="grey lighten-4 text-center rounded-lg heading">
-                <p class="mt-3 text-h6">Hello, thank you for visiting the site.<br>
+            <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="grey lighten-4 text-center rounded-lg heading">
+                <p class="mt-3 text-sm-h6">Hello, thank you for visiting the site.<br>
                 I'm a web app developer based in Japan!</p>
             </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-10" id="profile_locate">
         <v-spacer></v-spacer>
-        <v-col cols="4" class="text-left">
+        <v-col cols="10" xs="10" sm="10" md="2" lg="2" xl="2" class="mt-2"><img class="profile_img rounded-circle ml-5" src="../assets/Profile_Image.jpg" alt="プロフィール画像"></v-col>
+        <v-col cols="10" xs="10" sm="10" md="4" lg="4" xl="4" class="ml-16">
             <h1 class="myname">Keito Shitara</h1>
             <p class="text-h6">(Developer)</p>
-            <p class="text-h6 mt-3">
+            <p class="text-sm-h6 mt-3">
             I'm a web developer in Tokyo.<br>
             This is my self-introduction site, made with Vue.js and Vuetify.</p>
         </v-col>
-        <v-col cols="2" class="mt-2"><img class="profile_img rounded-circle" src="../assets/Profile_Image.jpg" alt="プロフィール画像"></v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-10" id="work_locate">
         <v-spacer></v-spacer>
-        <v-col cols="6" class="work">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="work">
             <h2 class="pl-2">Work</h2>
             <p class="mt-2 font-weight-medium">&emsp;I'm an full-stack developer based in Tokyo with a passion for building digital services he wants.<br>
             I'm a fledgling developer who has just joined an in-house development company, and in the past I have built systems for e-commerce sites.<br>
             I'm now developing SEO analysis tools in-house, and I'm learning every day to catch up on my development work.<br>
             In my private life, I am active mainly in web production and development.</p>
-            <v-btn><router-link to="/works" class="text-decoration-none black--text">Works</router-link></v-btn>
+            <v-btn class="text-decoration-none black--text" @click="scrollPortofolio()">Works</v-btn>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-10" id="background_locate">
         <v-spacer></v-spacer>
-        <v-col cols="6" class="bio">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="bio">
             <h2 class="pl-2">Background</h2>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="2" class="bio-contents">
+        <v-col cols="2" xs="2" sm="2" md="2" lg="2" xl="2" class="bio-contents">
             <h3 class="pl-2">1996</h3>
         </v-col>
-        <v-col cols="4">
+        <v-col cols="8" xs="8" sm="8" md="4" lg="4" xl="4">
             <p class="mt-1">Born in Nagoya(名古屋), Japan</p>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-0">
         <v-spacer></v-spacer>
-        <v-col cols="2" class="bio-contents">
+        <v-col cols="2" xs="2" sm="2" md="2" lg="2" xl="2" class="bio-contents">
             <h3 class="pl-2">2015</h3>
         </v-col>
-        <v-col cols="4">
+        <v-col cols="8" xs="8" sm="8" md="4" lg="4" xl="4">
             <p class="mt-1">Graduated from Nagoya Institute of Technology with a Master's degree in Materials Science and Engineering.</p>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-0">
         <v-spacer></v-spacer>
-        <v-col cols="2" class="bio-contents">
+        <v-col cols="2" xs="2" sm="2" md="2" lg="2" xl="2" class="bio-contents">
             <h3 class="pl-2">2019</h3>
         </v-col>
-        <v-col cols="4">
+        <v-col cols="8" xs="8" sm="8" md="4" lg="4" xl="4">
             <p class="mt-1">Joined a Toyota Group company as a material developer</p>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-0">
         <v-spacer></v-spacer>
-        <v-col cols="2" class="bio-contents">
+        <v-col cols="2" xs="2" sm="2" md="2" lg="2" xl="2" class="bio-contents">
             <h3 class="pl-2">2022</h3>
         </v-col>
-        <v-col cols="4">
+        <v-col cols="8" xs="8" sm="8" md="4" lg="4" xl="4">
             <p class="mt-1">Worked as a web developer in the product development section of a company engaged in DM business.</p>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row id="likes_locate">
         <v-spacer></v-spacer>
-        <v-col cols="6" class="likes">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="likes">
             <h2 class="pl-2">Likes🖤</h2>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="6" class="like-contents">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="like-contents">
             <p class="pl-2">Music, Web Development, Youtube, Commics, Anime, Apex, Movie, Japanese Chess</p>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row id="skill_locate">
         <v-spacer></v-spacer>
-        <v-col cols="6" class="skill">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="skill">
             <h2 class="pl-2">Skill</h2>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="6" class="skill-contents">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="skill-contents">
             <p class="pl-2">HTML, CSS, JavaScript, jQuery, Sass, Bootstrap, Vue.js, Nuxt.js, Vuetify, PHP, Laravel, WordPress</p>
             <p class="font-weight-medium">&emsp;I usually use PHP and Laravel for backend development at work, so PHP and Laravel web development is my specialty. <br>
             I also sometimes work on front-end development, in which case I mainly use Vue.js and Nuxt.js.</p>
@@ -112,14 +112,14 @@
     </v-row>
     <v-row id="portofolio_locate">
         <v-spacer></v-spacer>
-        <v-col cols="6" class="portofolio">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="portofolio">
             <h2 class="pl-2">Website Production Portfolio</h2>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="6" class="portofolio-contents">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="portofolio-contents text-center">
             <div class="portofolio-contents-heading">
             <h3 class="text-center">iSara</h3>
             </div>
@@ -132,7 +132,7 @@
     </v-row>
     <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="6" class="portofolio-contents">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="portofolio-contents text-center">
             <div class="portofolio-contents-heading">
             <h3 class="text-center">SEO Consulting</h3>
             </div>
@@ -145,7 +145,7 @@
     </v-row>
     <v-row>
         <v-spacer></v-spacer>
-        <v-col cols="6" class="portofolio-contents">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="portofolio-contents text-center">
             <div class="portofolio-contents-heading">
             <h3 class="text-center">LIKSON</h3>
             </div>
@@ -158,17 +158,17 @@
     </v-row>
     <v-row id="services_locate">
         <v-spacer></v-spacer>
-        <v-col cols="6" class="services">
+        <v-col cols="10" xs="10" sm="10" md="6" lg="6" xl="6" class="services">
             <h2 class="pl-2">Services</h2>
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
     <v-row class="mt-10">
         <v-spacer></v-spacer>
-        <v-col cols="2" class="grey lighten-4">
+        <v-col cols="4" xs="4" sm="4" md="2" lg="2" xl="2" class="grey lighten-4">
             <h3 class="pl-2"><v-icon class="mr-2">mdi-xml</v-icon>Coding</h3>
         </v-col>
-        <v-col cols="4" class="grey lighten-4">
+        <v-col cols="8" xs="8" sm="8" md="4" lg="4" xl="4" class="grey lighten-4">
             <p class="mt-1 font-weight-medium">&emsp;I perform appropriate coding based on the design. 
             We also provide SEO-conscious coding that is compatible with responsive design.<br> 
             We also handle modifications appropriately.</p>
@@ -177,10 +177,10 @@
     </v-row>
     <v-row class="mt-10">
         <v-spacer></v-spacer>
-        <v-col cols="2" class="services-contents grey lighten-4">
+        <v-col cols="5" xs="5" sm="5" md="2" lg="2" xl="2" class="services-contents grey lighten-4">
             <h3 class="pl-2"><v-icon class="mr-2">mdi-wordpress</v-icon>WordPress</h3>
         </v-col>
-        <v-col cols="4" class="grey lighten-4">
+        <v-col cols="7" xs="7" sm="7" md="4" lg="4" xl="4" class="grey lighten-4">
             <p class="mt-1 font-weight-medium">&emsp;We can install WordPress or convert your existing site to WordPress.<br> 
             Also, we can help you to customize the theme and set up forms, etc.</p>
         </v-col>
@@ -188,10 +188,10 @@
     </v-row>
     <v-row class="mt-10">
         <v-spacer></v-spacer>
-        <v-col cols="3" class="services-contents grey lighten-4">
+        <v-col cols="5" xs="5" sm="5" md="2" lg="2" xl="2" class="services-contents grey lighten-4">
             <h3 class="pl-2"><v-icon class="mr-2">mdi-remote-desktop</v-icon>Web Development</h3>
         </v-col>
-        <v-col cols="3" class="grey lighten-4">
+        <v-col cols="7" xs="7" sm="7" md="4" lg="4" xl="4" class="grey lighten-4">
             <p class="mt-1 font-weight-medium">&emsp;I can do web development programming.<br>
             The main development languages are PHP and JavaScript.<br>
             I am also proficient in Laravel and Vue.js as frameworks.</p>
@@ -203,7 +203,15 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default{
+    methods: {
+        scrollPortofolio: function() {
+            var portofolioLocate = $('#portofolio_locate').offset().top;
+            $(window).scrollTop(portofolioLocate - 150);
+        },
+    }
 };
 </script>
 
@@ -263,6 +271,12 @@ export default{
 .services > h2 {
     width: 110px;
     border-bottom: 2px solid black;
+}
+
+@media(max-width: 480px) {
+.portofolio-contents > .work-item > a > img {
+        width: 60vw;
+}
 }
 
 </style>
